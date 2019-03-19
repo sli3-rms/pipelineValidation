ad4d4143df31f63cde73a812be862af0ea812f23
Igor Kozel <ikozel@caml113518.rms.com>
2019-03-19 15:01:45 -0700

    first commit

---
+++ service/deployment/infrastructure/environment/main.tf
@@ -0,0 +1,4 @@
+// Stub main.tf
+// Generic infrastructure files. All specific resources should be included in
+// <name>.tf Example: sql.tf would have all the required resource definitions for SQL resources
+
---
+++ service/deployment/infrastructure/environment/sql.tf
@@ -0,0 +1,6 @@
+#stub sql filename
+#module "postgres" {
+#  source  = "git@github.com:RMS/tf-aws-rds"
+#  db_type = "postgres"
+#}
+
