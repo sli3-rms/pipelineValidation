5a35fcccd2dd4b009e91a8fc907eb798f5f7fa80
Felix Deschamps <felix.deschamps@rms.com>
2019-05-04 20:42:55 -0700

    Removing unneeded files

--- rest-service/deployment/infrastructure/environment/main.tf
+++
@@ -1,4 +0,0 @@
-// Stub main.tf
-// Generic infrastructure files. All specific resources should be included in
-// <name>.tf Example: sql.tf would have all the required resource definitions for SQL resources
-
